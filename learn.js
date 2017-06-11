c96bdb277790c8f59c7c0759751f62cc0b518bef
Michael Zhao <mzhaoau@gmail.com>
2017-06-11 15:10:56 +1000

    fully functional

--- learn.js
+++ learn.js
@@ -3,22 +3,23 @@ var Network = synaptic.Network;
 
 var FPS = 25;
 
-var GENERATION_SIZE = 12;
-var SELECT_SIZE = 4;
+var GENERATION_SIZE = 16;
+var SELECT_SIZE = 6;
 
 var DUCK_THRESHOLD = 0.45;
 var JUMP_THRESHOLD = 0.55;
 
-var MUTATION_FREQ = 0.1;
-
 var gen = [];
+var genNext = [];
 var best = [];
 var indNum = 0;
 var genNum = 0;
 
 for (var i = 0; i < GENERATION_SIZE; i++) {
 	gen.push(new Architect.Perceptron(3, 4, 4, 1));
-	gen[i] = mutate(gen[i], 1);
+	gen[i] = mutate(gen[i], 1, "connections", "weight");
+	gen[i] = mutate(gen[i], 1, "neurons", "bias");
+	genNext.push(gen[i]);
 }
 
 for (var i = 0; i < GENERATION_SIZE; i++) {
@@ -53,7 +54,7 @@ function testGen() {
 			makeMove();
 			best[indNum][1] = states.dist;
 		} else {
-			console.log("individual " + indNum + " travelled " + best[indNum][1]);
+			console.log(best[indNum][1]);
 			indNum++;
 			if (indNum >= GENERATION_SIZE) {
 				nextGen();
@@ -63,9 +64,12 @@ function testGen() {
 			hasRestarted = true;
 			document.getElementById("indvar").innerHTML = "ind = " + indNum;
 		}
+	} else if (!runner.activated) {
+		keyDownJump();
+		hasRestarted = true;
 	}
 	if (hasRestarted) {
-		setTimeout(testGen, 1000);
+		setTimeout(testGen, 1100);
 	} else {
 		setTimeout(testGen, 1000/FPS);
 	}
@@ -79,40 +83,49 @@ function nextGen() {
 	for (var i = 0; i < GENERATION_SIZE; i++) {
 		var a = randBetween(0, SELECT_SIZE);
 		var b = randBetween(0, SELECT_SIZE);
-		while (a != b) {
+		while (a == b) {
 			var b = randBetween(0, SELECT_SIZE);
 		}
-		gen[i] = crossOver(gen[best[a][0]], gen[best[b][0]]);
-		gen[i] = mutate(gen[i], 30/(best[a][1] + best[b][1]) + 0.05);
+		var mutProb = 20/(best[a][1] + best[b][1]) + 0.15;
+		console.log("crossing over between " + best[a][0] + " and " + best[b][0]);
+		console.log("mutProb = " + mutProb);
+		genNext[i] = crossOver(gen[best[a][0]], gen[best[b][0]], "neurons", "bias");
+		genNext[i] = mutate(genNext[i], mutProb, "connections", "weight");
+		genNext[i] = mutate(genNext[i], mutProb, "neurons", "bias");
 	}
-	best = [];
 	for (var i = 0; i < GENERATION_SIZE; i++) {
-		best.push([i, 0]);
+		best[i] = [i, 0];
 	}
 	genNum++;
 	document.getElementById("genvar").innerHTML = "gen = " + genNum;
+	gen = genNext;
 }
 
-function crossOver(a, b) {
+function crossOver(a, b, x, y) {
 	a = a.toJSON();
 	b = b.toJSON();
+	if (Math.random() < 0.5) {
+		var temp = a;
+		a = b;
+		b = temp;
+	}
 	var c = b;
-	var p = randBetween(1, a.connections.length);
-	c.connections = a.connections.slice(0, p).concat(b.connections.slice(p, b.connections.elength));
+	var p = randBetween(1, a[x].length);
+	c[x] = a[x].slice(0, p).concat(b[x].slice(p, b[x].length));
 	return Network.fromJSON(c);
 }
 
-function mutate(a, f) {
+function mutate(a, f, x, y) {
 	a = a.toJSON();
-	for (var i = 0; i < a.connections.length; i++) {
+	for (var i = 0; i < a[x].length; i++) {
 		if (Math.random() < f) {
-			a.connections[i].weight = 2*Math.random() - 1;
+			a[x][i][y] += Math.random()*f*2 - f;
 		}
 	}
 	return Network.fromJSON(a);
 }
 
 function onDocumentLoad() {
-	setTimeout(testGen, 100);
+	setTimeout(testGen, 1000);
 }
 document.addEventListener("DOMContentLoaded", onDocumentLoad);
